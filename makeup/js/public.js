bdf2c59b1626510b20c0fafb3dbfb203ccd5442b
Tyro <2193844348@qq.com>
2022-04-25 06:25:34 +0000

    删除文件 makeup

--- makeup/js/public.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $('.nav-main ul li').click(function () {
-        $(this).children('a').css('color', '#e23435').parent('li').siblings().children('a').css('color','#555');
-    })
-})
